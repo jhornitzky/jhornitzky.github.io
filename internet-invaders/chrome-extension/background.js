197e9cf5d79d6be552fe2f60292f05eb8b481c56
James Hornitzky <james.hornitzky@gmail.com>
2023-12-12 22:03:13 +1100

    v2 internet-invaders. Looking quite snazzy now.

--- internet-invaders/chrome-extension/background.js
+++ internet-invaders/chrome-extension/background.js
@@ -1,5 +1,13 @@
 function runInternetInvaders() {
-    iiRunner.start();
+    chrome.storage.local.get("iiRunning", function (data) {
+        if (typeof data.iiRunning == "undefined" || data.iiRunning === false) {
+            chrome.storage.local.set({ iiRunning: true });
+            iiRunner.start();
+        } else {
+            chrome.storage.local.set({ iiRunning: false });
+            iiRunner.stop();
+        }
+    });
 }
 
 chrome.action.onClicked.addListener((tab) => {
--- internet-invaders/chrome-extension/ii-functions.js
+++ internet-invaders/chrome-extension/ii-functions.js
@@ -20,8 +20,12 @@ iiRunner = {
         div.appendChild(canvas);
         body.appendChild(div);
     },
+    afterDOMLoaded:function() {
+        //Everything that needs to happen after the DOM has initially loaded.
+    },
     startGame:function() {
         console.log(document.querySelector("#ii-container canvas"));
+        window.scrollTo(0,0);
 
         // initialize context
         kaboom({
@@ -30,38 +34,95 @@ iiRunner = {
         });
 
         //get elements for later
-        var images = document.getElementsByTagName('img');
-        var SPEED = 480;
+        //var images = document.getElementsByTagName('img');
+        var links = document.getElementsByTagName('a');
 
         scene("game", () => {
             
-            //add baddies (images)
+            /*
+            //add images
             for (var i = 0; i < images.length; i++) {
                 var img = images[i];
                 loadSprite(img.src, img.src);
                 var scaleImg = (img.getBoundingClientRect().width / img.naturalWidth != NaN) ? img.getBoundingClientRect().width / img.naturalWidth : 1;
                 add([
                     sprite(img.src),
-                    pos(img.getBoundingClientRect().left, img.getBoundingClientRect().top+400),
+                    pos(img.getBoundingClientRect().left, img.getBoundingClientRect().top),
                     scale(scaleImg),
-                    move(UP, SPEED),
+                    area(),
                     'image'
+                ]);
+            }*/
+            
+            //add links (portals)
+            loadSprite('portal', chrome.runtime.getURL("sprites/portal.jpeg"));
+            for (var i = 0; i < links.length; i++) {
+                var link = links[i];
+                if (link.getBoundingClientRect().top < 100) continue; 
+                add([
+                    sprite('portal'),
+                    pos(link.getBoundingClientRect().left, link.getBoundingClientRect().top),
+                    scale(0.05),
+                    area(),
+                    'link',
+                    {
+                        href: link.href
+                    }
                 ]);
             }
 
             //add player
-            /*
+            loadSprite('player', chrome.runtime.getURL("sprites/player.jpeg"));
             var player = add([
                 sprite("player"),
-                pos(width() / 2, height() / 2),
-                scale(0.5),
+                pos(0, 0),
+                scale(0.05),
+                area(),
                 "player"
             ]);
-            */
 
+            //player movement
+            const SPEED = 240;
 
+            onKeyDown("right", () => {
+                player.move(SPEED, 0)
+            })
+            onKeyDown("left", () => {
+                player.move(-SPEED, 0)
+            })
+            onKeyDown("up", () => {
+                player.move(0, -SPEED)
+            })
+            onKeyDown("down", () => {
+                player.move(0, SPEED)
+            })
+
+            //player collision with link
+            player.onCollide("link", (l) => {
+                window.location.href = l.href;
+            })
         });
         
         go("game");
+    },
+    stop:function() {
+        console.log("Stopping Internet Invaders");
+        var body = document.getElementsByTagName('body')[0];
+        var div = document.getElementById("ii-container");
+        body.removeChild(div);
+        iiRunner.hasStarted = false;
+    },
+    afterDOMLoaded:function() {
+        //Everything that needs to happen after the DOM has initially loaded.
+        chrome.storage.local.get("iiRunning", function (data) {
+            if (typeof data.iiRunning != "undefined" && data.iiRunning === true) {
+                iiRunner.start();
+            }
+        });
     }
+}
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', iiRunner.afterDOMLoaded);
+} else {
+    iiRunner.afterDOMLoaded();
 }
